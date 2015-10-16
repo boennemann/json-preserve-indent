c2dcd960f002541d564f2730e3011ee1f3b439c4
Stephan Bönnemann <stephan@boennemann.me>
2015-10-16 15:40:51 +0200

    feat: initial

---
+++ index.js
@@ -0,0 +1,17 @@
+var _ = require('lodash')
+
+module.exports = function (raw) {
+  raw = String(raw)
+  var ending = _.get(raw.match(/}(\n*)$/m), 1)
+  var indent = _.get(raw.match(/^[ \t]+/m), 0)
+
+  var data = JSON.parse(raw)
+
+  return {
+    get: _.bind(_.get, _, data),
+    set: _.bind(_.set, _, data),
+    format: function () {
+      return JSON.stringify(data, null, indent) + ending
+    }
+  }
+}
